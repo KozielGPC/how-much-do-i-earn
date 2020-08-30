6841bf96caa9a18d23dd46148fcbb29f805993f1
marciogpc <gpcgabriel0@gmail.com>
2020-08-30 17:25:14 -0300

    Updates

---
+++ precache-manifest.0fafac2f322c0ccfae9646ab3a516dc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f316da6896ea4613f130c3193e6b399",
+    "url": "/KozielGPC/how-much-do-i-earn/index.html"
+  },
+  {
+    "revision": "34e984b61135cac8dbee",
+    "url": "/KozielGPC/how-much-do-i-earn/static/css/main.dd586c3e.chunk.css"
+  },
+  {
+    "revision": "5ef45aa2502a19101ab1",
+    "url": "/KozielGPC/how-much-do-i-earn/static/js/2.2785729f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KozielGPC/how-much-do-i-earn/static/js/2.2785729f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e984b61135cac8dbee",
+    "url": "/KozielGPC/how-much-do-i-earn/static/js/main.c72b22c1.chunk.js"
+  },
+  {
+    "revision": "b69b0637c302d3c86be0",
+    "url": "/KozielGPC/how-much-do-i-earn/static/js/runtime-main.666c72f0.js"
+  }
+]);
